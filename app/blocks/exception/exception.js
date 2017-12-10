fa17ccae0f306ca98b80e3a69a85282e30e121c3
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-10 09:39:22 -0200

    linter in exception.js

--- app/blocks/exception/exception.js
+++ app/blocks/exception/exception.js
@@ -5,27 +5,25 @@
       return function(e) {
         let thrownDescription;
         let newMessage;
-        
+
         if (e.data) {
           thrownDescription = `\n${e.data}`;
           newMessage = message + thrownDescription;
         }
-        
+
         e.data = newMessage;
         logger.error(message, e);
-        
+
         return $q.reject(e);
       };
     }
-    
+
     const service = {
       catcher
     };
-    
+
     return service;
   }
-  
-  angular
-  .module('blocks.exception')
-  .factory('exception', exception);
+
+  angular.module('blocks.exception').factory('exception', exception);
 })();
