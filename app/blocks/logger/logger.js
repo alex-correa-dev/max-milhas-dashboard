54630cbc85fcdb8d6f42bedb19733782b0d87772
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-10 09:41:27 -0200

    linter in logger.js

--- app/blocks/logger/logger.js
+++ app/blocks/logger/logger.js
@@ -3,36 +3,34 @@
     function error(message, data) {
       $log.error(`Error: ${message}`, data);
     }
-  
+
     function info(message, data) {
       $log.info(`Info: ${message}`, data);
     }
-  
+
     function success(message, data) {
       $log.info(`Success: ${message}`, data);
     }
-  
+
     function warning(message, data) {
       $log.warn(`Warning: ${message}`, data);
     }
-    
+
     const service = {
       showToasts: true,
-      
+
       error,
       info,
       success,
       warning,
-      
+
       log: $log.log
     };
-    
+
     return service;
   }
-  
-  angular
-  .module('blocks.logger')
-  .factory('logger', logger);
-  
+
+  angular.module('blocks.logger').factory('logger', logger);
+
   logger.$inject = ['$log'];
-}());
+})();
