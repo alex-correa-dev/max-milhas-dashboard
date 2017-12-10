81b74aac33e809b0fae10cbf5693d7c33355099d
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-10 12:10:22 -0200

    only test task created in gulp/test.js

--- gulp/tests.js
+++ gulp/tests.js
@@ -1,6 +1,6 @@
 const ROOT_PATH = process.cwd();
 
-module.exports = function(packages, config) {
+module.exports = (packages, config) => {
   function log(msg) {
     if (typeof msg === 'object') {
       for (const item in msg) {
@@ -12,29 +12,44 @@ module.exports = function(packages, config) {
       packages.$.log(packages.$.colors.blue(msg));
     }
   }
+
   function startTests(singleRun, done) {
     function karmaCompleted(karmaResult) {
       log('Karma completed');
+
       if (karmaResult === 1) {
         done(`karma: tests failed with code ${karmaResult}`);
       } else {
         done();
       }
     }
-    const server = new packages.Server({
-      configFile: `${ROOT_PATH}/karma.conf.js`,
-      singleRun: !!singleRun
-    }, karmaCompleted);
+
+    const server = new packages.Server(
+      {
+        configFile: `${ROOT_PATH}/karma.conf.js`,
+        singleRun: !!singleRun
+      },
+      karmaCompleted
+    );
+
     server.start();
   }
+
   packages.gulp.task('check', () => {
     log('Analyzing source with Eslint');
-    return packages.gulp.src(config.alljs)
+
+    return packages.gulp
+      .src(config.alljs)
       .pipe(packages.eslint())
       .pipe(packages.eslint.format())
       .pipe(packages.eslint.failAfterError());
   });
+
   packages.gulp.task('test', ['check'], (done) => {
+    startTests(true, done);
+  });
+
+  packages.gulp.task('runtest', (done) => {
     startTests(true, done);
   });
 };
