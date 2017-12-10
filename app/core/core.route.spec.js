8e280d9af765632ce9cd441d5ea37bc34aed7417
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-10 09:39:02 -0200

    linter in core.route.spec.js

--- app/core/core.route.spec.js
+++ app/core/core.route.spec.js
@@ -3,29 +3,27 @@ describe('core', () => {
     let $state;
     let $rootScope;
     let $location;
-  
+
     beforeEach(module('max-milhas-dashboard.core'));
-  
-    beforeEach(
-      inject((_$state_, _$rootScope_, _$location_) => {
-        $state = _$state_;
-        $rootScope = _$rootScope_;
-        $location = _$location_;
-      })
-    );
-    
+
+    beforeEach(inject((_$state_, _$rootScope_, _$location_) => {
+      $state = _$state_;
+      $rootScope = _$rootScope_;
+      $location = _$location_;
+    }));
+
     it('dashboard should work with $state.go', () => {
       $state.go('dashboard');
       $rootScope.$apply();
       expect($state.is('dashboard'));
     });
-    
+
     it('should route / to the dashboard route', () => {
       $location.path('/');
       $rootScope.$apply();
       expect($state.is('dashboard'));
     });
-    
+
     it('should route /invalid to the otherwise (dashboard) route', () => {
       $location.path('/invalid');
       $rootScope.$apply();
