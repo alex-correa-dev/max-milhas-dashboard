af894549bc9a05ed1dd9ecba25613fb221e2b04a
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-10 12:06:59 -0200

    config.js created. It has config url for Max Milhas

---
+++ app/core/config.js
@@ -0,0 +1,17 @@
+(function() {
+  function maxMilhasConfig() {
+    return {
+      maxMilhasApi: {
+        url: 'https://flight-price-hmg.maxmilhas.com.br/search',
+        token:
+          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtYXhtaWxoYXMuY29tLmJyIiwiaWF0IjoxNTA5MTIwMT' +
+          'AxLCJleHAiOjE1MTA0MTYxMDEsImF1ZCI6InRlc3RlLWZyb250ZW5kIiwic3ViIjoidGVzdGUtZnJvbnRlbmQiLCJlb' +
+          'nYiOiJobWcifQ.nM6wMem6dxF0CcDlig5iA9az5ZfmbXDjq1e4ypZXwjU'
+      }
+    };
+  }
+
+  angular
+    .module('max-milhas-dashboard.core')
+    .factory('maxMilhasConfig', maxMilhasConfig);
+})();
