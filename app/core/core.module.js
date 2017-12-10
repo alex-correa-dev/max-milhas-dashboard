67b48c53721e775d45b23c8de109bb7a296bc835
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-09 22:12:21 -0200

    core module created

---
+++ app/core/core.module.js
@@ -0,0 +1,11 @@
+(function() {
+  angular
+  .module('max-milhas-dashboard.core', [
+    'ngSanitize',
+    'ui.router',
+    
+    'blocks.logger',
+    'blocks.router',
+    'blocks.exception'
+  ]);
+})();
