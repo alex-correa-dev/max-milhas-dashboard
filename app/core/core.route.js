4afd9a5f9c4f07308636e07baccc8a77aa1e15fa
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-09 22:12:38 -0200

    core route created

---
+++ app/core/core.route.js
@@ -0,0 +1,11 @@
+(function() {
+  /* @ngInject */
+  function appRun(routerHelper) {
+    const otherwise = '/';
+    routerHelper.configureStates([], otherwise);
+  }
+  
+  angular
+  .module('max-milhas-dashboard.core')
+  .run(appRun);
+})();
