f6c79b7b923eb44583802a0174e1aeb8f1ef7d41
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-10 12:06:35 -0200

    linter in clear.js

--- gulp/clean.js
+++ gulp/clean.js
@@ -1,4 +1,4 @@
-module.exports = function(packages) {
+module.exports = (packages) => {
   packages.gulp.task('clean', () =>
     packages.gulp.src(['dist/', '.tmp', './coverage', 'report']).pipe(packages.clean()));
 };
