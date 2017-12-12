4a018ffe1036f4e2bb21e823eec368a3d845a291
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-12 18:13:35 -0200

    app.module updated with prices-alert

--- app/app.module.js
+++ app/app.module.js
@@ -2,6 +2,7 @@
   angular.module('max-milhas-dashboard', [
     'max-milhas-dashboard.core',
     'max-milhas-dashboard.dashboard',
-    'max-milhas-dashboard.search-header'
+    'max-milhas-dashboard.search-header',
+    'max-milhas-dashboard.prices-alert'
   ]);
 })();
