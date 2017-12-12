ae09be1a7abd49543b40c2cdda766ba91363d0c7
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-12 19:38:30 -0200

    results-filter.directive.js created

---
+++ app/results-filter/results-filter.directive.js
@@ -0,0 +1,18 @@
+(function() {
+  function resultsFilter() {
+    const directive = {
+      scope: {},
+      restrict: 'E',
+      controller: 'resultsFilterCtrl',
+      controllerAs: 'vm',
+      bindToController: true,
+      templateUrl: 'app/results-filter/results-filter.html'
+    };
+
+    return directive;
+  }
+
+  angular
+    .module('max-milhas-dashboard.results-filter')
+    .directive('resultsFilter', resultsFilter);
+})();
