d03198572eb46af97ece8d8d3b692b6edf5f1129
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-12 15:31:01 -0200

    config.js created to translate material calendar to portuguese

--- app/core/config.js
+++ app/core/config.js
@@ -1,17 +1,58 @@
 (function() {
-  function maxMilhasConfig() {
-    return {
-      maxMilhasApi: {
-        url: 'https://flight-price-hmg.maxmilhas.com.br/search',
-        token:
-          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtYXhtaWxoYXMuY29tLmJyIiwiaWF0IjoxNTA5MTIwMT' +
-          'AxLCJleHAiOjE1MTA0MTYxMDEsImF1ZCI6InRlc3RlLWZyb250ZW5kIiwic3ViIjoidGVzdGUtZnJvbnRlbmQiLCJlb' +
-          'nYiOiJobWcifQ.nM6wMem6dxF0CcDlig5iA9az5ZfmbXDjq1e4ypZXwjU'
+  const core = angular.module('max-milhas-dashboard.core');
+
+  core.config(mdDateCalendar);
+
+  mdDateCalendar.$inject = ['$mdDateLocaleProvider', '$windowProvider'];
+  /* @ngInject */
+  function mdDateCalendar($mdDateLocaleProvider, $windowProvider) {
+    const $window = $windowProvider.$get();
+
+    $mdDateLocaleProvider.shortMonths = [
+      'Jan',
+      'Fev',
+      'Mar',
+      'Abr',
+      'Mai',
+      'Jun',
+      'Jul',
+      'Ago',
+      'Set',
+      'Out',
+      'Nov',
+      'Dez'
+    ];
+    $mdDateLocaleProvider.shortDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
+    $mdDateLocaleProvider.formatDate = function(date) {
+      const indexMonths = {
+        0: 'Janeiro',
+        1: 'Fevereiro',
+        2: 'Março',
+        3: 'Abril',
+        4: 'Maio',
+        5: 'Junho',
+        6: 'Julho',
+        7: 'Agosto',
+        8: 'Setembro',
+        9: 'Outubro',
+        10: 'Novembro',
+        11: 'Dezembro'
+      };
+
+      if (date) {
+        const mobileScreenSize = 480;
+
+        const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+        const monthIndex =
+          $window.innerWidth < mobileScreenSize
+            ? indexMonths[date.getMonth()].substring(0, 3)
+            : indexMonths[date.getMonth()];
+        const year = date.getFullYear();
+
+        return `${day} ${monthIndex} ${year}`;
       }
+      return '';
     };
   }
-
-  angular
-    .module('max-milhas-dashboard.core')
-    .factory('maxMilhasConfig', maxMilhasConfig);
 })();
