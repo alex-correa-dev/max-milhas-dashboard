8d70faa67feeb93c77db9160f65566043edd3bb7
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-12 19:47:02 -0200

    results-filter.controller.spec.js created

---
+++ app/results-filter/results-filter.controller.spec.js
@@ -0,0 +1,36 @@
+describe('resultsFilterCtrl', () => {
+  let controller;
+  let $rootScope;
+  let $mdBottomSheet;
+
+  beforeEach(module('max-milhas-dashboard.core'));
+  beforeEach(module('max-milhas-dashboard.results-filter'));
+
+  beforeEach(
+    inject((_$controller_, _$rootScope_, _$mdBottomSheet_) => {
+      $mdBottomSheet = _$mdBottomSheet_;
+      $rootScope = _$rootScope_;
+      $rootScope.$apply();
+
+      controller = _$controller_('resultsFilterCtrl', { $mdBottomSheet });
+    })
+  );
+
+  describe('resultsFilterCtrl controller: unit tests', () => {
+    it('controller should be created successfully', () => {
+      expect(controller).toBeDefined();
+    });
+
+    it('verify if function showResultsFilterList was correctly called', () => {
+      spyOn($mdBottomSheet, 'show');
+      const args = {
+        templateUrl: 'app/results-filter/results-filter-list.html',
+        controller: 'resultsFilterListCtrl'
+      };
+
+      controller.showResultsFilterList();
+
+      expect($mdBottomSheet.show).toHaveBeenCalledWith(args);
+    });
+  });
+});
