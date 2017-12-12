60fc3de03bbb383c12ee6d3d4eb94e1be7d86b2c
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-12 15:35:32 -0200

    dashboard route created

---
+++ app/dashboard/dashboard.route.js
@@ -0,0 +1,27 @@
+(function() {
+  angular.module('max-milhas-dashboard.dashboard').run(appRun);
+
+  /* @ngInject */
+  function appRun(routerHelper) {
+    routerHelper.configureStates(getStates());
+  }
+
+  function getStates() {
+    return [
+      {
+        state: 'dashboard',
+        config: {
+          url: '/',
+          templateUrl: 'app/dashboard/dashboard.html',
+          controller: 'Dashboard',
+          controllerAs: 'vm',
+          title: 'dashboard',
+          settings: {
+            nav: 1,
+            content: '<i class="fa fa-dashboard"></i> Dashboard'
+          }
+        }
+      }
+    ];
+  }
+})();
