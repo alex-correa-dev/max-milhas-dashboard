6ebcd7acde6162f165ed546d79849c7f3657b526
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-13 01:43:13 -0200

    search-header.controller.js has support to list

--- app/search-header/search-header.controller.js
+++ app/search-header/search-header.controller.js
@@ -2,11 +2,20 @@
   /* @ngInject */
   function searchHeaderCtrl(airportsService) {
     const vm = this;
+    vm.loading = false;
+    vm.numbersList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    vm.classesList = ['Classe econômica', 'Classe Executiva'];
 
     function init() {
-      return airportsService.getAirportsList().then((airportsList) => {
-        vm.airportsList = airportsList;
-        return airportsList;
+      return airportsService.getAirportsList().then((airports) => {
+        vm.airportsList = Object.keys(airports).map(airportFlightId => {
+          const nameAirport = 0;
+          return airports[airportFlightId][nameAirport];
+        });
+
+        vm.airports = airports;
+
+        return vm.airportsList;
       });
     }
 
@@ -20,7 +29,15 @@
       if (vm.airportsList && query) {
         return vm.airportsList.filter(createFilterFor(query));
       }
+
       return vm.airportsList;
+    };
+
+    vm.getFlightsAirlines = function() {
+      vm.loading = true;
+      return vm.getFlightsAirlinesList().finally(() => {
+        vm.loading = false;
+      });
     };
 
     vm.init = init;
