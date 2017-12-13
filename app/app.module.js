f1c429d6a57c1aab09900e01d0ea236e50a73288
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-13 01:45:39 -0200

    flights-list added in app.module.js

--- app/app.module.js
+++ app/app.module.js
@@ -4,6 +4,7 @@
     'max-milhas-dashboard.dashboard',
     'max-milhas-dashboard.search-header',
     'max-milhas-dashboard.prices-alert',
-    'max-milhas-dashboard.results-filter'
+    'max-milhas-dashboard.results-filter',
+    'max-milhas-dashboard.flights-list'
   ]);
 })();
