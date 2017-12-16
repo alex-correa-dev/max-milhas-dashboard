b75a4e9179cb9fc8635d3cfe1d8b67c78d30fd26
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-15 22:02:02 -0200

    gulpfile updated with serveTask and watchTask

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,38 @@
 const ROOT_PATH = process.cwd();
 
 const $ = require('gulp-util');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
 const clean = require('gulp-clean');
 const eslint = require('gulp-eslint');
 const gulp = require('gulp');
+const livereload = require('gulp-livereload');
+const ngAnnotate = require('gulp-ng-annotate');
+const nodemon = require('gulp-nodemon');
+const runSequence = require('run-sequence');
 const { Server } = require('karma');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 
 const cleanTask = require(`${ROOT_PATH}/gulp/clean`);
 const testsTask = require(`${ROOT_PATH}/gulp/tests`);
+const serveTask = require(`${ROOT_PATH}/gulp/serve`);
+const watchTask = require(`${ROOT_PATH}/gulp/watch`);
 
 const packages = {
   $,
+  autoprefixer,
+  babel,
   clean,
   eslint,
   gulp,
-  Server
+  livereload,
+  ngAnnotate,
+  nodemon,
+  runSequence,
+  sass,
+  Server,
+  sourcemaps
 };
 
 const config = {
@@ -22,8 +40,17 @@ const config = {
     './app/**/*.js',
     './gulp/*.js',
     './*.js'
-  ]
+  ],
+  allClientFiles: './app/**/*',
+  allJsFiles: './app/**/*.js',
+  allTestFiles: './app/**/*.spec.js',
+  allTestHelpers: './app/test-helpers/**/*',
+  mainFile: './index.html',
+  tmpDest: '.tmp',
+  watchServer: ['bower.json', 'package.json', 'gulpfile.js']
 };
 
 cleanTask(packages);
 testsTask(packages, config);
+serveTask(packages, config);
+watchTask(packages);
