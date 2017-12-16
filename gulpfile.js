af64f08ffa152323fc055abd25abeb60c61269aa
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-15 23:30:34 -0200

    buildTask added in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,10 @@ const $ = require('gulp-util');
 const autoprefixer = require('gulp-autoprefixer');
 const babel = require('gulp-babel');
 const clean = require('gulp-clean');
+const cleanCSS = require('gulp-clean-css');
 const eslint = require('gulp-eslint');
 const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
 const livereload = require('gulp-livereload');
 const ngAnnotate = require('gulp-ng-annotate');
 const nodemon = require('gulp-nodemon');
@@ -13,26 +15,33 @@ const runSequence = require('run-sequence');
 const { Server } = require('karma');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const useref = require('gulp-useref');
 
 const cleanTask = require(`${ROOT_PATH}/gulp/clean`);
 const testsTask = require(`${ROOT_PATH}/gulp/tests`);
 const serveTask = require(`${ROOT_PATH}/gulp/serve`);
 const watchTask = require(`${ROOT_PATH}/gulp/watch`);
+const buildTask = require(`${ROOT_PATH}/gulp/build`);
 
 const packages = {
   $,
   autoprefixer,
   babel,
   clean,
+  cleanCSS,
   eslint,
   gulp,
+  htmlmin,
   livereload,
   ngAnnotate,
   nodemon,
   runSequence,
   sass,
   Server,
-  sourcemaps
+  sourcemaps,
+  uglify,
+  useref
 };
 
 const config = {
@@ -45,6 +54,7 @@ const config = {
   allJsFiles: './app/**/*.js',
   allTestFiles: './app/**/*.spec.js',
   allTestHelpers: './app/test-helpers/**/*',
+  distDest: '.dist',
   mainFile: './index.html',
   tmpDest: '.tmp',
   watchServer: ['bower.json', 'package.json', 'gulpfile.js']
@@ -54,3 +64,4 @@ cleanTask(packages);
 testsTask(packages, config);
 serveTask(packages, config);
 watchTask(packages);
+buildTask(packages, config);
