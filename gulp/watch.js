e2b9c5c585f4db699f9f7887bf95c99e1c37de98
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-17 14:02:22 -0200

    update task watch to sass

--- gulp/watch.js
+++ gulp/watch.js
@@ -1,6 +1,6 @@
 module.exports = (packages) => {
   packages.gulp.task('watch', () => {
-    packages.gulp.watch('./app/**/*.scss', ['sassdev']);
+    packages.gulp.watch('./app/**/*.scss', ['copydistdev', 'sassdev']);
     packages.gulp.watch('./app/**/*.{js,css,html}', ['copydistdev']);
     packages.gulp.watch('./index.html', ['copydistdev']);
   });
