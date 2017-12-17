9663483f20fb1a15c8d6346b0d02258b98f64baa
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2017-12-17 21:52:18 -0200

    details-table-cell.controller.js created

---
+++ app/flights-list/table-cells-content/details-table-cell/details-table-cell.controller.js
@@ -0,0 +1,8 @@
+(function() {
+  /* @ngInject */
+  function detailsTableCellCtrl() {}
+
+  angular
+    .module('max-milhas-dashboard.flights-list')
+    .controller('detailsTableCellCtrl', detailsTableCellCtrl);
+})();
